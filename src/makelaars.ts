9705fd7bc02999523b28ff0366fdb990f7cbfc73
Francis Behnen <15660621+FrancisBehnen@users.noreply.github.com>
2026-05-11 20:11:13 +0000

    feat: add van Silfhout (rentals) and Verra makelaars

--- src/makelaars.ts
+++ src/makelaars.ts
@@ -2,6 +2,13 @@ import { Makelaar, PlatformType } from "./makelaar";
 
 export const makelaars: Makelaar[] = [
   {
+    name: "van Silfhout & Hogetoorn",
+    url: "https://www.vansilfhout.nl/",
+    scrapeUrl:
+      "https://www.vansilfhout.nl/woningaanbod/?fwp_status=te-huur&fwp_locaties=delfgauw%2Cdelft%2Cden-hoorn%2Crijswijk&fwp_huurprijs=0%2C1500.00",
+    platformType: PlatformType.VanSilfhoutEnHogetoorn,
+  },
+  {
     name: "van Daal Makelaardij",
     url: "https://vandaalmakelaardij.nl/",
     scrapeUrl:
@@ -12,6 +19,12 @@ export const makelaars: Makelaar[] = [
     name: "Björnd Makelaardij",
     url: "https://bjornd.nl/",
     scrapeUrl: "https://www.bjornd.nl/nl/realtime-listings/consumer",
+    platformType: PlatformType.RealtimeListingsJson,
+  },
+  {
+    name: "Verra Makelaars",
+    url: "https://www.verra.nl/",
+    scrapeUrl: "https://www.verra.nl/nl/realtime-listings/consumer",
     platformType: PlatformType.RealtimeListingsJson,
   },
 ];
